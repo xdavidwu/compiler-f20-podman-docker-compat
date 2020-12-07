5b41680223e0b3d3f55f3826df0e36d62e3574ac
xdavidwu <xdavidwuph@gmail.com>
2020-12-07 19:43:00 +0800

    add --user root:root
    it seems like --userns keep-id implies run with that uid:gid on newer
    releases

--- docker.c
+++ docker.c
@@ -4,20 +4,22 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	char **new_argv = malloc(sizeof(char *) * (argc + 3));
+	char **new_argv = malloc(sizeof(char *) * (argc + 5));
 	assert(new_argv);
 	int i = 1;
 	int injected = 0;
 	new_argv[0] = argv[0];
 	for (; i < argc; i++) {
-		new_argv[i + injected * 2] = argv[i];
+		new_argv[i + injected * 4] = argv[i];
 		if (!strcmp("run", argv[i]) && !injected) {
 			new_argv[i + 1] = "--userns";
 			new_argv[i + 2] = "keep-id";
+			new_argv[i + 3] = "--user";
+			new_argv[i + 4] = "root:root";
 			injected = 1;
 		}
 	}
-	new_argv[i + injected * 2] = NULL;
+	new_argv[i + injected * 4] = NULL;
 	assert(execvp("podman", new_argv) == 0);
 	free(new_argv);
 	return 0;
